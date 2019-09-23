4668c11141e744608497c0626b13b3455dce99f7
cnngimenez <none@none>
2019-09-23 00:36:29 -0300

    Use jmp instead of rjmp in vector table.

--- libraries/vector-atmega2560-inc.asm
+++ libraries/vector-atmega2560-inc.asm
@@ -21,63 +21,63 @@
 ;; Vector table for the AVR ATmega 2560.
 
 .org 0
-    rjmp RESET       ; Reset Handler 
-    rjmp INT0        ; IRQ0 Handler 
-    rjmp INT1        ; IRQ1 Handler 
-    rjmp INT2        ; IRQ2 Handler 
-    rjmp INT3        ; IRQ3 Handler 
-    rjmp INT4        ; IRQ4 Handler 
-    rjmp INT5        ; IRQ5 Handler 
-    rjmp INT6        ; IRQ6 Handler 
-    rjmp INT7        ; IRQ7 Handler 
-    rjmp PCINT0      ; PCINT0 Handler 
-    rjmp PCINT1      ; PCINT1 Handler 
-    rjmp PCINT2      ; PCINT2 Handler 
-    rjmp WD          ; Watchdog Timeout Handler 
-    rjmp TIM2_COMPA  ; Timer2 CompareA Handler 
-    rjmp TIM2_COMPB  ; Timer2 CompareB Handler 
-    rjmp TIM2_OVF    ; Timer2 Overflow Handler 
-    rjmp TIM1_CAPT   ; Timer1 Capture Handler 
-    rjmp TIM1_COMPA  ; Timer1 CompareA Handler 
-    rjmp TIM1_COMPB  ; Timer1 CompareB Handler 
-    rjmp TIM1_COMPC  ; Timer1 CompareC Handler 
-    rjmp TIM1_OVF    ; Timer1 Overflow Handler 
-    rjmp TIM0_COMPA  ; Timer0 CompareA Handler 
-    rjmp TIM0_COMPB  ; Timer0 CompareB Handler 
-    rjmp TIM0_OVF    ; Timer0 Overflow Handler 
-    rjmp SPI_STC     ; SPI Transfer Complete Handler 
-    rjmp USART0_RXC  ; USART0 RX Complete Handler 
-    rjmp USART0_UDRE ; USART0,UDR Empty Handler 
-    rjmp USART0_TXC  ; USART0 TX Complete Handler 
-    rjmp ANA_COMP    ; Analog Comparator Handler 
-    rjmp ADC         ; ADC Conversion Complete Handler 
-    rjmp EE_RDY      ; EEPROM Ready Handler 
-    rjmp TIM3_CAPT   ; Timer3 Capture Handler 
-    rjmp TIM3_COMPA  ; Timer3 CompareA Handler 
-    rjmp TIM3_COMPB  ; Timer3 CompareB Handler 
-    rjmp TIM3_COMPC  ; Timer3 CompareC Handler 
-    rjmp TIM3_OVF    ; Timer3 Overflow Handler 
-    rjmp USART1_RXC  ; USART1 RX Complete Handler 
-    rjmp USART1_UDRE ; USART1,UDR Empty Handler 
-    rjmp USART1_TXC  ; USART1 TX Complete Handler 
-    rjmp TWI         ; 2-wire Serial Handler 
-    rjmp SPM_RDY     ; SPM Ready Handler 
-    rjmp TIM4_CAPT   ; Timer4 Capture Handler 
-    rjmp TIM4_COMPA  ; Timer4 CompareA Handler 
-    rjmp TIM4_COMPB  ; Timer4 CompareB Handler 
-    rjmp TIM4_COMPC  ; Timer4 CompareC Handler 
-    rjmp TIM4_OVF    ; Timer4 Overflow Handler 
-    rjmp TIM5_CAPT   ; Timer5 Capture Handler 
-    rjmp TIM5_COMPA  ; Timer5 CompareA Handler 
-    rjmp TIM5_COMPB  ; Timer5 CompareB Handler 
-    rjmp TIM5_COMPC  ; Timer5 CompareC Handler 
-    rjmp TIM5_OVF    ; Timer5 Overflow Handler 
-    rjmp USART2_RXC  ; USART2 RX Complete Handler 
-    rjmp USART2_UDRE ; USART2,UDR Empty Handler 
-    rjmp USART2_TXC  ; USART2 TX Complete Handler 
-    rjmp USART3_RXC  ; USART3 RX Complete Handler 
-    rjmp USART3_UDRE ; USART3,UDR Empty Handler 
-    rjmp USART3_TXC  ; USART3 TX Complete Handler 		
+    jmp RESET       ; Reset Handler 
+    jmp INT0        ; IRQ0 Handler 
+    jmp INT1        ; IRQ1 Handler 
+    jmp INT2        ; IRQ2 Handler 
+    jmp INT3        ; IRQ3 Handler 
+    jmp INT4        ; IRQ4 Handler 
+    jmp INT5        ; IRQ5 Handler 
+    jmp INT6        ; IRQ6 Handler 
+    jmp INT7        ; IRQ7 Handler 
+    jmp PCINT0      ; PCINT0 Handler 
+    jmp PCINT1      ; PCINT1 Handler 
+    jmp PCINT2      ; PCINT2 Handler 
+    jmp WD          ; Watchdog Timeout Handler 
+    jmp TIM2_COMPA  ; Timer2 CompareA Handler 
+    jmp TIM2_COMPB  ; Timer2 CompareB Handler 
+    jmp TIM2_OVF    ; Timer2 Overflow Handler 
+    jmp TIM1_CAPT   ; Timer1 Capture Handler 
+    jmp TIM1_COMPA  ; Timer1 CompareA Handler 
+    jmp TIM1_COMPB  ; Timer1 CompareB Handler 
+    jmp TIM1_COMPC  ; Timer1 CompareC Handler 
+    jmp TIM1_OVF    ; Timer1 Overflow Handler 
+    jmp TIM0_COMPA  ; Timer0 CompareA Handler 
+    jmp TIM0_COMPB  ; Timer0 CompareB Handler 
+    jmp TIM0_OVF    ; Timer0 Overflow Handler 
+    jmp SPI_STC     ; SPI Transfer Complete Handler 
+    jmp USART0_RXC  ; USART0 RX Complete Handler 
+    jmp USART0_UDRE ; USART0,UDR Empty Handler 
+    jmp USART0_TXC  ; USART0 TX Complete Handler 
+    jmp ANA_COMP    ; Analog Comparator Handler 
+    jmp ADC         ; ADC Conversion Complete Handler 
+    jmp EE_RDY      ; EEPROM Ready Handler 
+    jmp TIM3_CAPT   ; Timer3 Capture Handler 
+    jmp TIM3_COMPA  ; Timer3 CompareA Handler 
+    jmp TIM3_COMPB  ; Timer3 CompareB Handler 
+    jmp TIM3_COMPC  ; Timer3 CompareC Handler 
+    jmp TIM3_OVF    ; Timer3 Overflow Handler 
+    jmp USART1_RXC  ; USART1 RX Complete Handler 
+    jmp USART1_UDRE ; USART1,UDR Empty Handler 
+    jmp USART1_TXC  ; USART1 TX Complete Handler 
+    jmp TWI         ; 2-wire Serial Handler 
+    jmp SPM_RDY     ; SPM Ready Handler 
+    jmp TIM4_CAPT   ; Timer4 Capture Handler 
+    jmp TIM4_COMPA  ; Timer4 CompareA Handler 
+    jmp TIM4_COMPB  ; Timer4 CompareB Handler 
+    jmp TIM4_COMPC  ; Timer4 CompareC Handler 
+    jmp TIM4_OVF    ; Timer4 Overflow Handler 
+    jmp TIM5_CAPT   ; Timer5 Capture Handler 
+    jmp TIM5_COMPA  ; Timer5 CompareA Handler 
+    jmp TIM5_COMPB  ; Timer5 CompareB Handler 
+    jmp TIM5_COMPC  ; Timer5 CompareC Handler 
+    jmp TIM5_OVF    ; Timer5 Overflow Handler 
+    jmp USART2_RXC  ; USART2 RX Complete Handler 
+    jmp USART2_UDRE ; USART2,UDR Empty Handler 
+    jmp USART2_TXC  ; USART2 TX Complete Handler 
+    jmp USART3_RXC  ; USART3 RX Complete Handler 
+    jmp USART3_UDRE ; USART3,UDR Empty Handler 
+    jmp USART3_TXC  ; USART3 TX Complete Handler 		
 
 ;; .org 0x72
 	
